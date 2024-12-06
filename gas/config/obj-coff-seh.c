00b15bbc09c06865d1cb467d1abfa1f52c8dbddd
Evgeny Karpov <eukarpov@gmail.com>
2024-12-06 18:33:51 +0100

    Add aarch64 SEH codes

--- gas/config/obj-coff-seh.c
+++ gas/config/obj-coff-seh.c
@@ -578,6 +578,72 @@ obj_coff_seh_endprologue (int what ATTRIBUTE_UNUSED)
   } 
 }
 
+static void
+obj_coff_seh_startepilogue (int what ATTRIBUTE_UNUSED)
+{
+  symbolS *epilogue_start_addr;
+  expressionS exp;
+
+  if (!verify_context (".seh_startepilogue")
+      || !seh_validate_seg (".seh_startepilogue"))
+    return;
+  demand_empty_rest_of_line ();
+
+  if (seh_get_target_kind () == seh_kind_arm64)
+  {
+    epilogue_start_addr = symbol_temp_new_now ();
+    exp.X_op = O_subtract;
+    exp.X_add_symbol = epilogue_start_addr;
+    exp.X_op_symbol = seh_ctx_cur->start_addr;
+    exp.X_add_number = 0;
+
+    if (resolve_expression (&exp) && exp.X_op == O_constant)
+    {
+      seh_ctx_cur->arm64_ctx.epilogue_scopes[seh_ctx_cur->arm64_ctx.epilogue_scopes_count].epilogue_start_offset
+        = exp.X_add_number / 4;
+      seh_ctx_cur->arm64_ctx.epilogue_scopes[seh_ctx_cur->arm64_ctx.epilogue_scopes_count].reserved = 0;
+      /* optimization to reuse unwind codes from the prologue   */
+      seh_ctx_cur->arm64_ctx.epilogue_scopes[seh_ctx_cur->arm64_ctx.epilogue_scopes_count].epilogue_start_index
+        = seh_ctx_cur->arm64_ctx.unwind_codes_byte_count;
+      seh_ctx_cur->arm64_ctx.epilogue_scopes_count++;
+    }
+    else
+      as_bad (_(".seh_startepilogue in a different section from .seh_proc"));
+  }
+}
+
+static void
+obj_coff_seh_endepilogue(int what ATTRIBUTE_UNUSED)
+{
+  if (!verify_context (".seh_endepilogue")
+      || !seh_validate_seg (".seh_endepilogue"))
+    return;
+
+  /* End code */
+  seh_arm64_unwind_code *end_element = seh_arm64_get_unwind_element ();
+
+  if (end_element == NULL)
+  {
+    as_warn (_("no unwind element available."));
+    return;
+  }
+
+  end_element->end.code = ARM64_UNW_END;
+  end_element->type = end;
+  seh_ctx_cur->arm64_ctx.unwind_codes_byte_count++;
+
+  demand_empty_rest_of_line ();
+}
+
+static void
+obj_coff_seh_endfunclet(int what ATTRIBUTE_UNUSED)
+{
+  if (!verify_context (".seh_endfunclet")
+      || !seh_validate_seg (".seh_endfunclet"))
+    return;
+  demand_empty_rest_of_line ();
+}
+
 /* End-of-file hook.  */
 
 void
@@ -759,6 +825,386 @@ obj_coff_seh_save (int what)
     }
 
   seh_x64_make_prologue_element (code, reg, off);
+}
+
+/* ARM64 save_reg handlers   */
+static void
+obj_coff_seh_save_reg (int what)
+{
+  int reg;
+  offsetT off;
+  char name_end;
+  char *symbol_name = NULL;
+  seh_arm64_unwind_code *arm64_element = NULL;
+
+  switch (what)
+  {
+    case 0:
+      if (!verify_context_and_target (".seh_save_reg", seh_kind_arm64)
+        || !seh_validate_seg (".seh_save_reg"))
+      return;
+      break;  
+    case 1:
+      if (!verify_context_and_target (".seh_save_reg_x", seh_kind_arm64)
+        || !seh_validate_seg (".seh_save_reg_x"))
+      return;
+      break;
+    case 2:
+      if (!verify_context_and_target (".seh_save_regp", seh_kind_arm64)
+        || !seh_validate_seg (".seh_save_regp"))
+      return;
+      break;
+    case 3:
+      if (!verify_context_and_target (".seh_save_regp_x", seh_kind_arm64)
+        || !seh_validate_seg (".seh_save_regp_x"))
+      return;
+      break;
+    case 4:
+      if (!verify_context_and_target (".seh_save_lrpair", seh_kind_arm64)
+        || !seh_validate_seg (".seh_save_lrpair"))
+      return;
+      break;
+    case 5:
+      if (!verify_context_and_target (".seh_save_fregp", seh_kind_arm64)
+        || !seh_validate_seg (".seh_save_fregp"))
+      return;
+      break;
+    case 6:
+      if (!verify_context_and_target (".seh_save_fregp_x", seh_kind_arm64)
+        || !seh_validate_seg (".seh_save_fregp_x"))
+      return;
+      break;
+    case 7:
+      if (!verify_context_and_target (".seh_save_freg", seh_kind_arm64)
+        || !seh_validate_seg (".seh_save_freg"))
+      return;
+      break;
+    case 8:
+      if (!verify_context_and_target (".seh_save_freg_x", seh_kind_arm64)
+        || !seh_validate_seg (".seh_save_freg_x"))
+      return;
+      break;
+    default:
+      as_bad (_("invalid pseudo operation."));
+      return;
+  }
+
+  SKIP_WHITESPACE ();
+  name_end = get_symbol_name (&symbol_name);
+
+  reg = atoi(symbol_name + 1);
+
+  (void) restore_line_pointer (name_end);
+
+  if (!skip_whitespace_and_comma (1))
+    return;
+
+  off = get_absolute_expression ();
+  demand_empty_rest_of_line ();
+
+  if (reg < 0)
+    return;
+  if (off < 0)
+    {
+      as_bad (_("offset is negative"));
+      return;
+    }
+
+  if ((arm64_element = seh_arm64_get_unwind_element ()) == NULL)
+  {
+    as_warn (_("no unwind element available."));
+    return;
+  }
+
+  switch (what)
+  {
+    case 0:
+      arm64_element->save_reg.code = ARM64_UNW_SAVEREG;
+
+      /* Only so many bits available, so we must encode the values   */
+      arm64_element->save_reg.reg = reg - 19;
+      arm64_element->save_reg.offset = off / 8;
+
+      arm64_element->type = save_reg;
+      break;
+    case 1:
+      arm64_element->save_reg_x.code = ARM64_UNW_SAVEREGX;
+
+      /* Only so many bits available, so we must encode the values   */
+      arm64_element->save_reg_x.reg = reg - 19;
+      arm64_element->save_reg_x.offset = off / 8 - 1;
+
+      arm64_element->type = save_reg_x;
+      break;
+    case 2:
+      arm64_element->save_regp.code = ARM64_UNW_SAVEREGP;
+
+      /* Only so many bits available, so we must encode the values   */
+      arm64_element->save_regp.reg = reg - 19;
+      arm64_element->save_regp.offset = off / 8;
+
+      arm64_element->type = save_regp;
+      break;
+    case 3:
+      arm64_element->save_regp_x.code = ARM64_UNW_SAVEREGPX;
+
+      /* Only so many bits available, so we must encode the values   */
+      arm64_element->save_regp_x.reg = reg - 19;
+      arm64_element->save_regp_x.offset = off / 8 - 1;
+
+      arm64_element->type = save_regp_x;
+      break;
+    case 4:
+      arm64_element->save_lrpair.code = ARM64_UNW_SAVELRPAIR;
+
+      /* Only so many bits available, so we must encode the values   */
+      arm64_element->save_lrpair.reg = reg / 2 - 19;
+      arm64_element->save_lrpair.offset = off / 8;
+
+      arm64_element->type = save_lrpair;
+      break;
+    case 5:
+      arm64_element->save_fregp.code = ARM64_UNW_SAVEFREGP;
+
+      /* Only so many bits available, so we must encode the values   */
+      arm64_element->save_fregp.reg = reg - 8;
+      arm64_element->save_fregp.offset = off / 8;
+
+      arm64_element->type = save_fregp;
+      break;
+    case 6:
+      arm64_element->save_fregp_x.code = ARM64_UNW_SAVEFREGPX;
+
+      /* Only so many bits available, so we must encode the values   */
+      arm64_element->save_fregp_x.reg = reg - 8;
+      arm64_element->save_fregp_x.offset = off / 8 - 1;
+
+      arm64_element->type = save_fregp_x;
+      break;
+    case 7:
+      arm64_element->save_freg.code = ARM64_UNW_SAVEFREG;
+
+      /* Only so many bits available, so we must encode the values   */
+      arm64_element->save_freg.reg = reg - 8;
+      arm64_element->save_freg.offset = off / 8;
+
+      arm64_element->type = save_freg;
+      break;
+    case 8:
+      arm64_element->save_freg_x.code = ARM64_UNW_SAVEFREGX;
+
+      /* Only so many bits available, so we must encode the values   */
+      arm64_element->save_freg_x.reg = reg - 8;
+      arm64_element->save_freg_x.offset = off / 8 - 1;
+
+      arm64_element->type = save_freg_x;
+      break;
+    default:
+      as_bad (_("Invalid pseudo operation."));
+      break;
+  }
+
+  /*  all save_reg variations consume 2 bytes   */
+  seh_ctx_cur->arm64_ctx.unwind_codes_byte_count += 2;
+}
+
+/* ARM64 save_lplr handlers   */
+static void
+obj_coff_seh_save_fplr (int what)
+{
+  offsetT off;
+  seh_arm64_unwind_code *arm64_element = NULL;
+
+  if (what == 0)
+  {
+    if (!verify_context_and_target (".seh_save_fplr", seh_kind_arm64)
+        || !seh_validate_seg (".seh_save_fplr"))
+      return;
+  } else if (what == 1)
+  {
+    if (!verify_context_and_target (".seh_save_fplr_x", seh_kind_arm64)
+        || !seh_validate_seg (".seh_save_fplr_x"))
+      return;
+  } else if (what == 2)
+  {
+    if (!verify_context_and_target (".seh_save_r19r20_x", seh_kind_arm64)
+        || !seh_validate_seg (".seh_save_r19r20_x"))
+      return;
+  }
+
+  SKIP_WHITESPACE ();
+  off = get_absolute_expression ();
+  demand_empty_rest_of_line ();
+
+  if (off < 0)
+    {
+      as_bad (_("offset is negative"));
+      return;
+    }
+
+  if ((arm64_element = seh_arm64_get_unwind_element ()) == NULL)
+  {
+    as_warn (_("no unwind element available."));
+    return;
+  }
+
+  switch (what)
+  {
+    case 0:
+      arm64_element->save_fplr.code = ARM64_UNW_SAVEFPLR;
+      arm64_element->save_fplr.offset = off / 8;
+      arm64_element->type = save_fplr;
+      break;
+    case 1:
+      arm64_element->save_fplr.code = ARM64_UNW_SAVEFPLRX;
+      arm64_element->save_fplr.offset = off / 8 - 1;
+      arm64_element->type = save_fplr_x;
+      break;
+    case 2:
+      arm64_element->save_r19r20_x.code = ARM64_UNW_SAVER19R20X;
+      arm64_element->save_r19r20_x.offset = off / 8;
+      arm64_element->type = save_r19r20_x;
+      break;
+    default:
+      as_bad (_("Invalid pseudo operation."));
+      break;
+  }
+
+  seh_ctx_cur->arm64_ctx.unwind_codes_byte_count++;
+}
+
+/* ARM64 add_fp handler   */
+static void
+obj_coff_seh_add_fp (int what ATTRIBUTE_UNUSED)
+{
+  offsetT off;
+  seh_arm64_unwind_code *arm64_element = NULL;
+
+  if (!verify_context_and_target (".seh_add_fp", seh_kind_arm64)
+      || !seh_validate_seg (".seh_add_fp"))
+    return;
+
+  SKIP_WHITESPACE ();
+  off = get_absolute_expression ();
+  demand_empty_rest_of_line ();
+
+  if (off < 0)
+  {
+    as_bad (_("offset is negative"));
+    return;
+  }
+
+  if ((arm64_element = seh_arm64_get_unwind_element ()) == NULL)
+  {
+    as_warn (_("no unwind element available."));
+    return;
+  }
+
+  arm64_element->add_fp.code = ARM64_UNW_ADDFP;
+  arm64_element->add_fp.offset = off / 8;
+  arm64_element->type = add_fp;
+
+  seh_ctx_cur->arm64_ctx.unwind_codes_byte_count += 2;
+}
+
+/* ARM64 nop handler   */
+static void
+obj_coff_seh_nop (int what ATTRIBUTE_UNUSED)
+{
+  seh_arm64_unwind_code *arm64_element = NULL;
+
+  if (!verify_context_and_target (".seh_nop", seh_kind_arm64)
+      || !seh_validate_seg (".seh_nop"))	  
+    return;
+
+  SKIP_WHITESPACE ();
+  demand_empty_rest_of_line ();
+
+  if ((arm64_element = seh_arm64_get_unwind_element ()) == NULL)
+  {
+    as_warn (_("no unwind element available."));
+    return;
+  }
+
+  arm64_element->nop.code = ARM64_UNW_NOP;
+  arm64_element->type = nop;
+
+  seh_ctx_cur->arm64_ctx.unwind_codes_byte_count++;
+}
+
+/* ARM64 pac_sign_lr handler   */
+static void
+obj_coff_seh_pac_sign_lr (int what ATTRIBUTE_UNUSED)
+{
+  seh_arm64_unwind_code *arm64_element = NULL;
+
+  if (!verify_context_and_target (".seh_pac_sign_lr", seh_kind_arm64)
+      || !seh_validate_seg (".seh_pac_sign_lr"))
+    return;
+
+  SKIP_WHITESPACE ();
+  demand_empty_rest_of_line ();
+
+  if ((arm64_element = seh_arm64_get_unwind_element ()) == NULL)
+  {
+    as_warn (_("no unwind element available."));
+    return;
+  }
+
+  arm64_element->pac_sign_lr.code = ARM64_UNW_PACSIGNLR;
+  arm64_element->type = pac_sign_lr;
+
+  seh_ctx_cur->arm64_ctx.unwind_codes_byte_count++;
+}
+
+/* ARM64 set_fp handler   */
+static void
+obj_coff_seh_set_fp (int what ATTRIBUTE_UNUSED)
+{
+  seh_arm64_unwind_code *arm64_element = NULL;
+
+  if (!verify_context_and_target (".seh_set_fp", seh_kind_arm64)
+      || !seh_validate_seg (".seh_set_fp"))
+    return;
+
+  SKIP_WHITESPACE ();
+  demand_empty_rest_of_line ();
+
+  if ((arm64_element = seh_arm64_get_unwind_element ()) == NULL)
+  {
+    as_warn (_("no unwind element available."));
+    return;
+  }
+
+  arm64_element->set_fp.code = ARM64_UNW_SETFP;
+  arm64_element->type = set_fp;
+
+  seh_ctx_cur->arm64_ctx.unwind_codes_byte_count++;
+}
+
+/* ARM64 save_next handler   */
+static void
+obj_coff_seh_save_next (int what ATTRIBUTE_UNUSED)
+{
+  seh_arm64_unwind_code *arm64_element = NULL;
+
+  if (!verify_context_and_target (".seh_save_next", seh_kind_arm64)
+      || !seh_validate_seg (".seh_save_next"))
+    return;
+
+  SKIP_WHITESPACE ();
+  demand_empty_rest_of_line ();
+
+  if ((arm64_element = seh_arm64_get_unwind_element ()) == NULL)
+  {
+    as_warn (_("no unwind element available."));
+    return;
+  }
+
+  arm64_element->save_next.code = ARM64_UNW_SAVENEXT;
+  arm64_element->type = save_next;
+
+  seh_ctx_cur->arm64_ctx.unwind_codes_byte_count++;
 }
 
 /* Add a stack-allocation token to current context.  */
