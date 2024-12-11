c478608a2d039d66e9558908d16db30cf5c43e70
Martin Vejbora <mvejbora@microsoft.com>
2024-12-11 15:53:34 +0100

    Guard big object code with OBJ_COFF instead of TE_PE or TE_PEP

--- gas/config/tc-aarch64.c
+++ gas/config/tc-aarch64.c
@@ -10110,7 +10110,7 @@ aarch64_after_parse_args (void)
 #endif
 }
 
-#if defined (TE_PE) || defined (TE_PEP)
+#ifdef OBJ_COFF
 /* Use big object file format.  */
 static int use_big_obj = 0;
 #endif
@@ -10498,7 +10498,7 @@ const struct option md_longopts[] = {
 #ifdef OPTION_EL
   {"EL", no_argument, NULL, OPTION_EL},
 #endif
-# if defined (TE_PE) || defined (TE_PEP)
+#ifdef OBJ_COFF
   {"mbig-obj", no_argument, NULL, OPTION_MBIG_OBJ},
 #endif
   {NULL, no_argument, NULL, 0}
@@ -11068,7 +11068,7 @@ md_parse_option (int c, const char *arg)
       break;
 #endif
 
-# if defined (TE_PE) || defined (TE_PEP)
+#ifdef OBJ_COFF
     case OPTION_MBIG_OBJ:
       use_big_obj = 1;
       break;
@@ -11147,7 +11147,7 @@ md_show_usage (FILE * fp)
   -EL                     assemble code for a little-endian cpu\n"));
 #endif
 
-#if defined (TE_PE) || defined (TE_PEP)
+#ifdef OBJ_COFF
   fprintf (fp, _("\
   -mbig-obj               generate big object files\n"));
 #endif
